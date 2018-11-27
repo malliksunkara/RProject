374da583d4570f6684e64d0ac60e5c98e570ae99
Mallik <malli.cloud@gmail.com>
2018-11-27 11:26:07 +0530

    describes some of the functions in R

---
+++ functions.R
@@ -0,0 +1,66 @@
+# integer division
+c(2,3,5,7) %/% c(2,3)
+
+c(2,3,5,7) / c(2,3) //observe the output with above 
+
+
+# modulo division operator
+
+c(2,3,4,5,6) %% 2
+
+c(2,3,3,4) %% c(2,3)
+
+# maximum values
+
+max(1.2, 3.4, -7.8)
+
+
+max(c(1,2.3,4.7,8))
+
+min(c(3,4,5.9,9.0))
+
+# other functions
+
+abs()
+sqrt()
+round()
+floor()
+ceiling()
+sum()
+prod()
+log()
+log10() 
+log2()   
+exp()
+sin()
+cos()
+tan()
+asin()
+acos()
+atan()
+sinh()
+cosh()
+tanh()
+asinh()
+acosh()
+atanh()
+
+sum(c(2,3,4,5))
+
+
+prod(c(2,3,5,7))
+
+round(1.23)
+
+# finding the type of variable
+x <- 2
+mode(x)
+
+yy <- '0'
+mode(yy)
+
+y <- "o"
+mode(y)
+
+z <- "hello"
+mode(z)
